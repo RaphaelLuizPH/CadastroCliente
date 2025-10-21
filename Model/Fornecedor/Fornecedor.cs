9b3e4961150d40869d92fc1582c7aedf590e408c
RaphaelLuizPH <130625114+RaphaelLuizPH@users.noreply.github.com>
2025-10-21 14:46:05 -0300

    Add project files.

---
+++ Model/Fornecedor/Fornecedor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CadastroCliente.Model.Fornecedor
+{
+    [PrimaryKey("Id")]
+    public class Fornecedor
+    {
+      
+        public Guid Id { get; set; }
+
+        [Length(2, 100)]
+        public string Nome { get; set; } = string.Empty;
+
+        [Length(14,14)]
+        public string Cnpj { get; set; } = string.Empty;
+
+        [AllowedValues("Comércio", "Serviço", "Indústria")]
+        public string Segmento { get; set; }
+
+        public string Endereco { get; set; } = string.Empty;
+
+    }
+}
